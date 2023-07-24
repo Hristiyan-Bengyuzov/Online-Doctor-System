19a6f9e447da5314a73d23fe812d34878514f943
Hris <xrisko06@abv.bg>
2023-07-24 15:42:45 +0300

    added an extension for getting user id

---
+++ OnlineDoctorSystem/OnlineDoctorSystem.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace OnlineDoctorSystem.Web.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetId(this ClaimsPrincipal user) => user?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    }
+}
