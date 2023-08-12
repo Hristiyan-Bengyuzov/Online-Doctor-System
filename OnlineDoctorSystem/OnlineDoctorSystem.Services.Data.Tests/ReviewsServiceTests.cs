28acbed9a951b3b700b7fbc2ce1078c6a03cf370
Hris <xrisko06@abv.bg>
2023-08-12 09:47:45 +0300

    added tests for ReviewsService

---
+++ OnlineDoctorSystem/OnlineDoctorSystem.Services.Data.Tests/ReviewsServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Moq;
+using OnlineDoctorSystem.Data;
+using OnlineDoctorSystem.Data.Models;
+using OnlineDoctorSystem.Services.Data.Interfaces;
+using OnlineDoctorSystem.Web.ViewModels.Reviews;
+
+namespace OnlineDoctorSystem.Services.Data.Tests
+{
+	[TestFixture]
+	public class ReviewsServiceTests
+	{
+		[Test]
+		public async Task AddReviewAsync_ShouldAddReviewToDoctorAndContext()
+		{
+			var options = new DbContextOptionsBuilder<OnlineDoctorDbContext>()
+				.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+				.Options;
+
+			using (var context = new OnlineDoctorDbContext(options))
+			{
+				var doctorsServiceMock = new Mock<IDoctorsService>();
+
+				var doctor = new Doctor();
+				doctorsServiceMock.Setup(s => s.GetDoctorByIdAsync(It.IsAny<string>())).ReturnsAsync(doctor);
+
+				var reviewsService = new ReviewsService(context, doctorsServiceMock.Object);
+				var model = new AddReviewFormModel
+				{
+					DoctorId = Guid.NewGuid().ToString(),
+					Rating = 5,
+					ReviewText = "Great doctor!"
+				};
+
+				await reviewsService.AddReviewAsync(model);
+
+				Assert.AreEqual(1, doctor.Reviews.Count);
+				Assert.AreEqual(1, context.Reviews.Count());
+			}
+		}
+	}
+}
--- OnlineDoctorSystem/OnlineDoctorSystem.Services.Data/SpecialtiesService.cs
+++ OnlineDoctorSystem/OnlineDoctorSystem.Services.Data/SpecialtiesService.cs
@@ -1,37 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OnlineDoctorSystem.Data;
+using OnlineDoctorSystem.Services.Data.Interfaces;
 using OnlineDoctorSystem.Web.ViewModels.Specialties;
 
-namespace OnlineDoctorSystem.Services.Data.Interfaces
+namespace OnlineDoctorSystem.Services.Data
 {
-    public class SpecialtiesService : ISpecialtiesService
-    {
-        private readonly OnlineDoctorDbContext context;
+	public class SpecialtiesService : ISpecialtiesService
+	{
+		private readonly OnlineDoctorDbContext context;
 
-        public SpecialtiesService(OnlineDoctorDbContext context)
-        {
-            this.context = context;
-        }
+		public SpecialtiesService(OnlineDoctorDbContext context)
+		{
+			this.context = context;
+		}
 
-        public async Task<IEnumerable<string>> AllSpecialtyNamesAsync() => await this.context.Specialties.Select(s => s.Name).ToListAsync();
+		public async Task<IEnumerable<string>> AllSpecialtyNamesAsync() => await this.context.Specialties.Select(s => s.Name).ToListAsync();
 
-        public async Task<string> GetSpecialtyNameByIdAsync(int id)
-        {
-            var specialty = await this.context.Specialties.FindAsync(id);
-            return specialty!.Name;
-        }
+		public async Task<string> GetSpecialtyNameByIdAsync(int id)
+		{
+			var specialty = await this.context.Specialties.FindAsync(id);
+			return specialty!.Name;
+		}
 
-        public async Task<IEnumerable<SpecialtyIndexViewModel>> GetAllSpecialties()
-        {
-            var specialties = await this.context.Specialties
-                .Select(s => new SpecialtyIndexViewModel
-                {
-                    Id = s.Id,
-                    Name = s.Name,
-                })
-                .ToListAsync();
+		public async Task<IEnumerable<SpecialtyIndexViewModel>> GetAllSpecialties()
+		{
+			var specialties = await this.context.Specialties
+				.Select(s => new SpecialtyIndexViewModel
+				{
+					Id = s.Id,
+					Name = s.Name,
+				})
+				.ToListAsync();
 
-            return specialties;
-        }
-    }
+			return specialties;
+		}
+	}
 }
